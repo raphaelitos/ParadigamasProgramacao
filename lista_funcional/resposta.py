577ed5bbada75fcdd964e231c48e9ad73b500bcb
Raphael Correia Dornelas <dornelasrapha@gmail.com>
2025-11-05 00:45:22 -0300

    até a 30, faltano 27

--- lista_funcional/resposta.py
+++ lista_funcional/resposta.py
@@ -9,7 +9,7 @@ def head(lista):
 def tail(lista):
     """Retorna todos os elementos de uma lista, exceto o primeiro."""
     if lista == []:
-        return None
+        return []
     return lista[1:]
 
 # Questão 3
@@ -274,13 +274,186 @@ def prime_factors(num):
 
 
 # Questão 21
+def split_aux(token, lista):
+    """Retorna a primeira fatia de lista delimitada por token."""
+    if(lista == [] or head(lista) == token):
+        return []
+    
+    return concat([head(lista)], split_aux(token, tail(lista)))
+
+def split_token(token, lista):
+    "Reparte lista em sublistas delimitadas por token."
+    if(lista == []):
+        return []
+
+    tok_list = split_aux(token, lista)
+    resto = lista[size_list(tok_list):]
+
+    return concat([tok_list], split_token(token, tail(resto)))
+
+#Questão 22
+def join_token(token, mat):
+    """Une as linhas de mat usando token como separador."""
+    if(size_list(mat) == 1):
+        return [token] + head(mat)
+    
+    if(size_list(mat) == 0):
+        return []
+    
+    conc = head(mat) + [token]
+    novo_item = concat(conc, head(tail(mat)))
+
+    return concat(novo_item, join_token(token, tail(tail(mat))))
+
+#Questão 23
+#definindo as proximas duas para nao usar slice
+def take_n(n, lista):
+        """Retorna os n primeiros elementos de lista."""
+        if (n == 0 or lista == []):
+            return []
+        return concat([head(lista)], take_n(n - 1, tail(lista)))
+
+def drop_n(n, lista):
+    """Descarta os n primeiros elementos de lista"""
+    if (n == 0 or lista == []):
+        return lista
+    return drop_n((n - 1), tail(lista))
+
+def split_half(lista):
+    """Retorna uma lista com duas sublistas correspondendo às metades de lista"""
+    if(lista == []):
+        return [[], []]
+    
+    i = size_list(lista)//2
+    a = take_n(i, lista)
+    b = drop_n(i, lista)
+    
+    return [a, b]
+
+#Questão 24
+def pyths(n):
+    """Retorna todas as triplas pitagóricas (x, y, z) com 1 <= x, y, z <= n."""
+    return [(x, y, z)
+            for x in range(1, n + 1)
+            for y in range(1, n + 1)
+            for z in range(1, n + 1)
+            if x * x + y * y == z * z]
+
+#Questão 25
+def fatores_aux(num, div):
+    if (num <= div):
+        return []
+    if((num % div) == 0):
+        return concat([div], fatores_aux(num, (div + 1)))
+    else:
+        return fatores_aux(num, (div + 1))
+
+def fatores(num):
+    """Retorna os fatores de num"""
+    if (-1 <= num <= 1):
+        return []
+    if(num < 0):
+        return fatores_aux((num * (-1)), 2)
+    
+    return [1] + fatores_aux(num, 2)
+
+def soma(lista):
+    """Soma os elementos de lista"""
+    if(lista == []):
+        return 0
+    
+    return head(lista) + soma(tail(lista))
+
+def eh_perfeito(num):
+    return num == soma(fatores(num))
+
+
+def perfects(n):
+    """Retorna a lista de numeros perfeitos em [1, n]"""
+    return [
+        x
+        for x in range(1, n+1)
+        if eh_perfeito(x)
+    ]
+
+#Questão 26
+def escalar(v, w):
+    """Calcula o produto escalar entre v e w"""
+    if(size_list(v) != size_list(w)):
+        return 0
+    #zip emparelha os itens de v e w par a par
+    return soma([x * y for x, y in zip(v, w)])
+
+
+#Questão 27
+
+
+#Questão 28
+def is_palindrome(palavra):
+    """Verifica se palavra eh um palindromo"""
+    l = list(palavra)
+    half_mat = split_half(l)
+    
+    if(size_list(l) % 2 == 0):
+        return half_mat[0] == inverte_lista(half_mat[1])
+    
+    return half_mat[0] == inverte_lista(tail(half_mat[1]))
+
+
+#Questão 29
+def compress(lista):
+    """Elimina duplicatas em lista"""
+    if(lista == []):
+        return []
+    if(size_list(lista) == 1):
+        return lista
+    
+    a = lista[0]
+    b = lista[1]
+
+    if(a == b):
+        return compress(tail(lista))
+    
+    return concat([a], compress(tail(lista)))
+
+    
+
+#Questão 30
+def take_while_equal(x, lista):
+    """Coleta em lista os elementos consecutivos a x iguais a ele"""
+    if lista == [] or head(lista) != x:
+        return []
+    return concat([head(lista)], take_while_equal(x, tail(lista)))
+
+def drop_while_equal(x, lista):
+    """Remove de lista os elementos consecutivos a x iguais a ele"""
+    if lista == [] or head(lista) != x:
+        return lista
+    return drop_while_equal(x, tail(lista))
+
+def pack(lista):
+    """Empacota duplicatas contidas em lista"""
+    if lista == []:
+        return []
+    
+    atual = head(lista)
+    grupo = take_while_equal(atual, lista)
+    resto = drop_while_equal(atual, lista)
+    
+    return concat([grupo], pack(resto))
+
+# Questão 31
+
+
+# Questão 32
 
 
 
 #--- testes ---#
-l1 = [1, 2, 3, 4, 5, 6, 7, 8]
-l2 = [1, 2, 3, 4, 5, 6, 7, 8]
+l1 = [1,2,2, 2, 2, 3, 3, 4, 5]
+l2 = [1, 2, 3,1, 4, 5, 1, 6, 7, 1, 8]
+l3 = [[10], [20,30], [40]]
 n = 24
 dic = ["arara","arreio","haskell","vaca","vacuo","velho","vermelho","vicio"]
 
-print(prime_factors(-8))
+print(pack(l1))
